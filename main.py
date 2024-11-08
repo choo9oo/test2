7cc495ed878c8f3bd1885e14eab5647c74500b04
choo9oo <ainganim21@gmail.com>
2024-11-08 17:22:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main():
     # 페이지 설정 (Streamlit 상단 바 구성)
     st.set_page_config(page_title="에너지", page_icon="🌻", layout="centered")  # 레이아웃을 설정하여 폰트 경고를 방지
     st.image('energy.png')  # 상단에 이미지를 표시
-    st.title("_:red[에너지 학습 도우미]_ 🏫")  # 제목 표시 (에너지 학습 도우미)
+    st.title("_:blue[지구촌의 평화와 발전 도우미]_ 🏫")  # 제목 표시 (사회 학습 도우미)
     st.header("😶주의! 이 챗봇은 참고용으로 사용하세요!", divider='rainbow')  # 주의사항 표시
 
     # 세션 상태 초기화
